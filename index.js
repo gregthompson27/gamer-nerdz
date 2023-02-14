bc738fd952f066d7301623f9e2bf6c12320a6ccd
Gregory Thompson <gregthompson27@gmail.com>
2023-02-14 20:50:34 +0000

    add getDistance method

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 (function(window, _) {
     window.myLibraryName = window.myLibraryName || {
-      numz: {},
+      numz: {
+        getDistance(pointA, pointB) {
+          const distanceX = pointB.x - pointA.x
+          const distanceY = pointB.y - pointA.y
+          const distance = Math.sqrt(distanceX ** 2 + distanceY ** 2)
+          return distance;
+        }
+      },
       phyz: {},
     };
   }(window, window._));
