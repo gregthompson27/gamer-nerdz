49aaedf1a2e1e07edd1488577c025be2bd7c4b26
Gregory Thompson <gregthompson27@gmail.com>
2023-03-21 21:28:30 +0000

    add rotation and angle methods

--- index.js
+++ index.js
@@ -6,6 +6,21 @@
           const distanceY = pointB.y - pointA.y
           const distance = Math.sqrt(distanceX ** 2 + distanceY ** 2)
           return distance;
+        },
+        degreesToRadians(degrees) {
+          return degrees * Math.PI / 180;
+        },
+        radiansToDegrees(radians) {
+          return radians * 180 / Math.PI;
+        },
+        getAngleDegrees(pointA, pointB) {
+          const distanceX = pointB.x - pointA.x;
+          const distanceY = pointB.y - pointA.y;
+          
+          const radians = Math.atan2(distanceY, distanceX);
+          const degrees = radians * 180 / Math.PI;
+          
+          return degrees;
         }
       },
       phyz: {},
